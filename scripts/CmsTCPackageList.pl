3b73bfed603427810a5a7903058b2d613f1e89e2
CMS Build <sha1-7f8a01e8cfe4f7786d42d9cf039f4c3fd2f5cce6@cern.ch>
2006-03-13 22:34:59 +0000

    This commit was manufactured by cvs2git to create tag 'Utilities- ReleaseScripts-V00-00-04'.
    Sprout from master 2006-03-03 17:57:22 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'Utilities/ReleaseScripts/TagLog'
    Cherrypick from master 2006-03-13 22:34:58 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'more options for CmsTCPackageList':
        scripts/CmsTCPackageList.pl

--- scripts/CmsTCPackageList.pl
+++ scripts/CmsTCPackageList.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 #
 # 
 #
@@ -12,21 +12,39 @@ use Getopt::Long;
 Getopt::Long::config('bundling_override');
  
 my %options;
-GetOptions(\%options,'rel=s');
+my @packs;
+GetOptions(\%options,'h','help','rel=s','pack=s'=>\@packs,'file=s');
 
-if ( !$options{'rel'} ) {
+if ( !$options{'rel'} || $options{'h'} || $options{'help'} ) {
     print "CmsTCPackageList.pl usage:\n";
-    print "    CmsTCPackageList.pl --rel <release>\n";
+    print "    CmsTCPackageList.pl --rel <release>\n \n";
+    print "Options:\n";
+    print "  --pack <packages> Print only for specified package(s)\n";
+    print "         (Either space separated in quotes or use multiple --pack options for multiple packages)\n";
+    print "  --file <file>   Send output to file\n";
     print "\n";
     exit;
 } 
 
 my $rel= $options{'rel'} ? $options{'rel'} : die "Need a release (--rel)";
 
+my %packages;
+my $onlySpecifiedPackages=0;
+#if ( $options{'pack'} ) {
+if ( @packs ) {
+    $onlySpecifiedPackages=1;
+    foreach (@packs )  {
+	my @spTmp=split(' ',$_,999);
+	foreach (@spTmp) {
+	    $packages{$_}=1;
+	}
+    }
+}
+
 my $user="cmstcreader";
 my $pass="CmsTC";
 
-open(CMSTCQUERY,"/usr/bin/wget -nv -O- 'http://${user}:${pass}\@cmsdoc.cern.ch/swdev/CmsTC/cgi-bin/CreateTagList?release=${rel}' |");
+open(CMSTCQUERY,"/usr/bin/wget -nv -o /dev/null -O- 'http://${user}:${pass}\@cmsdoc.cern.ch/swdev/CmsTC/cgi-bin/CreateTagList?release=${rel}' |");
 
 my %tags;
 while ( <CMSTCQUERY> ) {
@@ -40,10 +58,29 @@ while ( <CMSTCQUERY> ) {
 
 close CMSTCQUERY;
 
+my $filename= $options{'file'} ? $options{'file'} : "&STDOUT";
+open (OUTFILE,">$filename") or die "can not open output file $filename";
+
+my $hasAPackage=0;
 my $key;
 foreach $key (keys %tags) {
-    print "$key $tags{$key} \n";
+    if ( $key eq "-1" ) {
+# error condition.. missing release	
+# should be a better way to catch this
+	close OUTFILE;
+	print "Release $rel does not exist in CmsTC\n";
+	exit;
+    }
+    $hasAPackage++;
+    if ( ($onlySpecifiedPackages==0) || ($packages{$key}==1) ) {
+	print OUTFILE "$key $tags{$key} \n";
+    }
 }
+
+close OUTFILE;
+
+print "No packages found in release ${rel}\n" if ( $hasAPackage==0);
+
 
 
 
